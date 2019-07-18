37be3dd29602adbe4855d96db4c09d0dfe2d9faa
Ksenia <oksanastudent3@gmail.com>
2019-07-18 19:01:48 +0300

    Create Test

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,5 +1,9 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -16,7 +20,22 @@ public class LoginTest {
     @Test
     public void enterToTheSiteAndAssertion(){
         driver.get("http://nnmclub.to");
-
+        WebElement buttonAuthorization = driver.findElement(By
+                .xpath("//*[contains(text(),'Вход')]"));
+        buttonAuthorization.click();
+        WebElement login = driver.findElement(By.name("username"));
+        login.sendKeys("Ксения00788");
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("zadanie");
+        WebElement buttonLogin =driver.findElement(By.className("mainoption"));
+        buttonLogin.click();
+        Boolean isPresent = driver.findElements(By
+               .xpath("//*[contains(text(),'Выход [ Ксения00788 ]')]")).size()>0;
+        if (isPresent == true){
+            System.out.println("Вход на сайт был успешным");
+        }else {
+            System.out.println("Вход на сайт не был успешным");
+        }
     }
 
     @AfterTest
